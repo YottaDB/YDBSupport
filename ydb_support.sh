fe71a807b23b35cddd5451c21d088a462a867c66
Charles Hathaway <charles@yottadb.com>
2019-01-28 11:50:50 -0500

    Added log options, reworked usage, added option to not get logs

--- ydb_support.sh
+++ ydb_support.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # ydb_support.sh - gathers information related to the system, database, and core files if applicable
-# Copyright (C) 2018  YottaDB, LLC
 
+# Copyright (C) 2018-2019 YottaDB, LLC
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
@@ -15,23 +15,28 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-## For usage instructions, run with the --help flag
+##? Usage:
+##? ./ydb_support.sh [-f|--force] [-o|--outdir OUTPUT DIRECTORY]
+##?   [-p|--pid "PID OR CORE FILE"] [-h|--help]
+##?   [-l|--logs-since "JOURNALCTL TIME FORMAT"]
+##?   [-u|--logs-until "JOURNALCTL TIME FORMAT"]
+##?   [-n|--no-logs]
+##? 
+##? where:
+##?   -f|--force removes the output directory if it exists before starting, else an error will be emitted
+##?   -o|--outdir <directory> the output directory to store files in before compressing
+##?         DEFAULT ydb_support
+##?   -p|--pid <pid or core> the PID or core file of a YDB/GTM process to get information from
+##?   -h|--help displays this message
+##?   -l|--logs-since <time spec> passed to journalctl (if present) to control starting time of logs
+##?       DEFAULT: 2 hours ago
+##?   -u|--logs-until <time spec> passed to journalctl (if present) to control topping time of logs
+##?       DEFAULT: now
+##?   -n|--no-logs if present, no information from the system beyond what is required for processing -p is collected
 
 usage() {
-  cat <<EOF
-$(basename "$0") [-f|--force] [-o|--outdir OUTPUT DIRECTORY] \
-[-p|--pid PID OR CORE FILE] [-h|--help] [-l|--logs-since JOURNALCTL TIME FORMAT]
-
-where:
-  -f|--force removes the output directory if it exists before starting, else an error will be emitted
-  -o|--outdir <directory> the output directory to store files in before compressing
-        DEFAULT ydb_support
-  -p|--pid <pid or core> the PID or core file of a YDB/GTM process to get information from
-  -h|--help displays this message
-  -l|--logs-since <time spec> passed to journalctl (if present) to control starting time of logs
-      DEFAULT: 2 hours ago
-EOF
-exit 1
+  grep "##?" "$0" | grep -v grep | cut -d\  -f 2-
+  exit 1
 }
 
 run() {
@@ -48,11 +53,13 @@ run() {
 
 
 LOGS_SINCE="2 hours ago"
+LOGS_UNTIL="now"
 OUTDIR="ydb_support"
 FORCE=0
 PID=""
+NO_LOGS=0
 
-OPTS=$(getopt -o fo:p:hl: --long force,outdir,pid,help,logs-since -n 'parse-options' -- "$@")
+OPTS=$(getopt -o fo:p:hl:u:n --long force,outdir,pid,help,logs-since,logs-until,no-logs -n 'parse-options' -- "$@")
 
 eval set -- "$OPTS"
 
@@ -60,6 +67,16 @@ while true; do
   case "$1" in
     -f | --force)
       FORCE=1
+      shift
+      ;;
+    -l | --logs-since)
+      LOGS_SINCE="${2}"
+      shift
+      shift
+      ;;
+    -u | --logs-until)
+      LOGS_UNTIL="${2}"
+      shift
       shift
       ;;
     -h | --help)
@@ -74,6 +91,10 @@ while true; do
     -p | --pid)
       PID="${2}"
       shift
+      shift
+      ;;
+    -n | --no-logs)
+      NO_LOGS=1
       shift
       ;;
     --)
@@ -99,24 +120,67 @@ fi
 
 mkdir -p "${OUTDIR}" || exit 1
 
-echo "## Gathering system information"
-
-run uname -a > $OUTDIR/uname.txt 2>&1
-run lsb_release -a > $OUTDIR/lsb_release.txt 2>&1
-run cat /etc/os-release > $OUTDIR/os-release 2>&1
-
-echo "## Gathering system logs"
-
-journalctl=$(command -v journalctl)
-
-if [ "$journalctl" != "" ]; then
-  run journalctl --since "${LOGS_SINCE}" > $OUTDIR/journalctl.log
-else
-  # Limited support for this case; just grab the latest files from /var/log
-  run cp /var/log/syslog $OUTDIR/
+if [ "${NO_LOGS}" -eq "0" ]; then
+  echo "## Gathering system information"
+  
+  run uname -a > $OUTDIR/uname.txt 2>&1
+  run lsb_release -a > $OUTDIR/lsb_release.txt 2>&1
+  run cat /etc/os-release > $OUTDIR/os-release 2>&1
+  
+  echo "## Gathering system logs"
+  
+  journalctl=$(command -v journalctl)
+  
+  if [ "$journalctl" != "" ]; then
+    run journalctl --since "${LOGS_SINCE}" --until "${LOGS_UNTIL}" > $OUTDIR/journalctl.log
+  else
+    # Limited support for this case; just grab the latest files from /var/log
+    run cp /var/log/syslog $OUTDIR/
+  fi
+  
+  run dmesg > $OUTDIR/dmesg.log 2>&1
+  
+  
+  dist_dir="$ydb_dist"
+  
+  if [ "$dist_dir" = "" ]; then
+    dist_dir="$gtm_dist"
+  fi
+  
+  if [ "$dist_dir" = "" ]; then
+    echo "## Warning: neither ydb_dist nor gtm_dist environment variables are set, so we can not get database information"
+  else
+    echo "## Gathering information about database"
+  
+    run $dist_dir/mumps -r %XCMD 'write $ZVERSION' > $OUTDIR/zversion.txt 2>&1
+  
+    gbldir="$ydb_gbldir"
+  
+    if [ "${gbldir}" = "" ]; then
+      gbldir="$gtmgbldir"
+    fi
+  
+    if [ "${gbldir}" = "" ]; then
+      echo "## Warning: neither ydb_gbldir nor gbldir environment variables are set, so we can not get database information"
+    else
+      run echo "${gbldir}" > $OUTDIR/global_dir.txt
+      run $dist_dir/mumps -run GDE show -command > $OUTDIR/gde_show_command.txt 2>&1
+      version=$(cat $OUTDIR/zversion.txt)
+      mupip_dumpfhead_added_in="GT.M V6.3-001A"
+      if [ "$version" \< "$mupip_dumpfhead_added_in" ]; then
+        run ${dist_dir}/dse all -dump -all > $OUTDIR/dse_all_dump_all.txt 2>&1
+      else
+        run ${dist_dir}/mupip dumpfhead -reg '*' > $OUTDIR/mupip_dumpfhead.txt 2>&1
+      fi
+    fi
+  fi
+  
+  echo "## Getting filesystem information"
+  run df > $OUTDIR/df.txt 2>&1
+  run fdisk -l > $OUTDIR/fdisk.txt 2>&1
+  run grep ^/dev /etc/mtab > $OUTDIR/mtab.txt 2>&1
+  
 fi
-
-run dmesg > $OUTDIR/dmesg.log 2>&1
 
 if [ "${PID}" != "" ]; then
   if [ -e "${PID}" ]; then
@@ -140,50 +204,11 @@ if [ "${PID}" != "" ]; then
       gdb_arg="$gdb_arg -ex \"frame $i\" -ex \"info locals\" -ex \"info registers\""
     done
     gdb_arg="$gdb_arg -ex \"quit\""
-    run echo $gdb_arg | xargs -- gdb "${pid_exec}" "${PID}" >> $out_fn 2>&1
+    run echo $gdb_arg | run xargs -- gdb "${pid_exec}" "${PID}" >> $out_fn 2>&1
   else
     echo "## Warning: failed to find executable for $PID"
   fi
 fi
-
-dist_dir="$ydb_dist"
-
-if [ "$dist_dir" = "" ]; then
-  dist_dir="$gtm_dist"
-fi
-
-if [ "$dist_dir" = "" ]; then
-  echo "## Warning: neither ydb_dist nor gtm_dist environment variables are set, so we can not get database information"
-else
-  echo "## Gathering information about database"
-
-  run $dist_dir/mumps -r %XCMD 'write $ZVERSION' > $OUTDIR/zversion.txt 2>&1
-
-  gbldir="$ydb_gbldir"
-
-  if [ "${gbldir}" = "" ]; then
-    gbldir="$gtmgbldir"
-  fi
-
-  if [ "${gbldir}" = "" ]; then
-    echo "## Warning: neither ydb_gbldir nor gbldir environment variables are set, so we can not get database information"
-  else
-    run echo "${gbldir}" > $OUTDIR/global_dir.txt
-    run $dist_dir/mumps -run GDE show -command > $OUTDIR/gde_show_command.txt 2>&1
-    version=$(cat $OUTDIR/zversion.txt)
-    mupip_dumpfhead_added_in="GT.M V6.3-001A"
-    if [ "$version" \< "$mupip_dumpfhead_added_in" ]; then
-      run ${dist_dir}/dse all -dump -all > $OUTDIR/dse_all_dump_all.txt 2>&1
-    else
-      run ${dist_dir}/mupip dumpfhead -reg '*' > $OUTDIR/mupip_dumpfhead.txt 2>&1
-    fi
-  fi
-fi
-
-echo "## Getting filesystem information"
-run df > $OUTDIR/df.txt 2>&1
-run fdisk -l > $OUTDIR/fdisk.txt 2>&1
-run grep ^/dev /etc/mtab > $OUTDIR/mtab.txt 2>&1
 
 echo "## Done getting information, packing tarball"
 
